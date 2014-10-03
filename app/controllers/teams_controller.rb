705e7cf6275008ddbbb3e5c80dbb9e162866b2f3
Sarun Intaralawan <sarun.intaralawan@gmail.com>
2014-10-03 19:11:49 +0700

    Implement delete team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,6 +2,22 @@ class TeamsController < ApplicationController
 	def create
 		team = current_application.teams.new
 		team.save
+		app = team.application
+		app.revision += 1
+		app.save
+		redirect_to application_path
+	end
+
+	def destroy
+		team = Team.find_by(id: params[:id])
+		if team.application != current_application
+			flash[:danger] = "การกระทำที่น่าสงสัย กรุณาติดต่อผู้พัฒนา"
+		else
+			team.destroy
+			app = team.application
+			app.revision += 1
+			app.save
+		end
 		redirect_to application_path
 	end
 end
